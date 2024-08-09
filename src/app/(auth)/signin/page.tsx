18018fa673dc012590b415c6e7ff1f0670c272a5
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-08-09 16:28:14 +0545

    frontend: form ui added

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import FormComponent from "@/components/shared/form";
+import { useSearchParams } from "next/navigation";
+import { z } from "zod";
+
+export const formSchema = z.object({
+  studentId: z.string().regex(/PUR[0-9]{3}[A-Z]{3}[0-9]{3}/),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+  confirmPassword: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  gender: z.enum(["Male", "Female", "Other"]),
+  faculty: z.enum(["BEI", "BCT", "BME", "BCE", "BEL", "BAG", "BAR"]),
+  level: z.enum(["1st year", "2nd year", "3rd year", "4th year", "5th year"]),
+  role: z.enum(["voter", "candidate", "guest", "admin"]),
+});
+// studentId: "",
+//       password: "",
+//       confirmPassword: "",
+//       firstName: "",
+//       lastName: "",
+//       gender: "Male",
+//       faculty: "BEI",
+//       level: "1st year",
+
+const LoginPage = () => {
+  const searchParams = useSearchParams();
+  const type = searchParams.get("type");
+
+  return (
+    <>
+      <FormComponent type={type} />
+    </>
+  );
+};
+
+export default LoginPage;
